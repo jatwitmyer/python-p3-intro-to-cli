4032222f60a05e4c3874d19284908c66ee2a81a2
Jessica Twitmyer <jatwitmyer@gmail.com>
2023-10-13 12:18:21 -0400

    completes lab

--- lib/grade_reports.py
+++ lib/grade_reports.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
 
+def create_grade_report(student_grades):
+    with open('reports/grade_report.txt', 'a') as gr:
+        for grade in student_grades:
+            gr.write(grade + '\n')
+
+#shebang:
 if __name__ == '__main__':
-    pass
+    student_grades = []
+
+    grade = input("Student name, grade: ")
+    while grade:
+        student_grades.append(grade)
+        #end when no grade is entered:
+        grade = input("Student name, grade: ")
+
+    create_grade_report(student_grades)
+    
+#the shebang tells the command line that this program should be executed using the Python 3 interpreter
+
